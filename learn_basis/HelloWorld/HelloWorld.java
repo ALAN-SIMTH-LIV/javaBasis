5240cba9311ec30116977c129a34d148ab85ebcf
ALAN-SIMTH-LIV <146905929+ALAN-SIMTH-LIV@users.noreply.github.com>
2026-08-06 21:54:43 +0800

    刚刚开始，但不多

---
+++ learn_basis/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    //    javac HelloWorld.java 编译
+    //    java HelloWorld 运行
+    //    javac -encoding utf-8 编译时指定编码为utf-8
+    public static void main(String[] args)
+    /**
+     * 你好世界
+     * @param args 命令行参数
+     */
+    {
+        System.out.println("你好世界");
+    }
+}
+
