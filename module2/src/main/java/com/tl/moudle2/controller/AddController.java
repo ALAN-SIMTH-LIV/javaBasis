44b3390e2871f2a4616c53b5f8ca09e2f868c847
ALAN-SIMTH-LIV <146905929+ALAN-SIMTH-LIV@users.noreply.github.com>
2026-08-16 00:52:56 +0800

    Day3 学习 数组

---
+++ module2/src/main/java/com/tl/moudle2/controller/AddController.java
@@ -0,0 +1,17 @@
+package com.tl.moudle2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 注解，将类标记为控制器
+public class AddController {
+
+    @RequestMapping("/add") // 映射请求路径为/add
+    @ResponseBody // 注解，将方法返回值直接写入响应体
+    int add(int a,int b){ // 方法参数为a和b，返回值为int
+        return a+b;
+    }
+
+
+}
--- module2/src/main/java/com/tl/moudle2/controller/HelloWorldController.java
+++ module2/src/main/java/com/tl/moudle2/controller/HelloWorldController.java
@@ -1,18 +1,19 @@
-package com.tl.moudle2;
+package com.tl.moudle2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+// http://localhost:8080/hello?name=tl&age=18
 
 @Controller  // 注解，将类标记为控制器
 public class HelloWorldController {  // 控制器类
 
     @RequestMapping("/hello")  // 映射请求路径为/hello
     @ResponseBody  // 注解，将方法返回值直接写入响应体
-    String hi(){
-        return "Hello World";
+    String hi(String name){
+        return "Hello " + name;
     }
-
 
 
 }
